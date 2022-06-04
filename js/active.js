2375cb38e48cda4920e4238904824e29f9f95ae1
developeranikhossin <mdmanunbiswas321@gmail.com>
2022-06-05 02:45:53 +0600

    tayde site

---
+++ js/active.js
@@ -0,0 +1,26 @@
+$(document) .ready(function(){
+
+
+    /* project area */
+
+    $('.service-active').owlCarousel({
+        loop:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+
+
+
+
+
+})
