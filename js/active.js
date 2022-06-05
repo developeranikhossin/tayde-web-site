7f4457d75f5dd60838c88ca36db969a5b1b0731e
developeranikhossin <mdmanunbiswas321@gmail.com>
2022-06-05 19:19:51 +0600

    tayde site

--- js/active.js
+++ js/active.js
@@ -11,7 +11,7 @@ $(document) .ready(function(){
                 items:1
             },
             600:{
-                items:1
+                items:2
             },
             1000:{
                 items:3
